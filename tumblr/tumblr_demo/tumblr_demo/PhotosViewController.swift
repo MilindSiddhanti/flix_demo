35136ce6069ccea86c9596107f95b58b922a60b5
Milind Siddhanti <milindsiddhanti@utexas.edu>
2018-09-10 19:40:04 -0500

    code changes for tumblr assignment

--- tumblr/tumblr_demo/tumblr_demo/PhotosViewController.swift
+++ tumblr/tumblr_demo/tumblr_demo/PhotosViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource {
     
     var posts: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(PhotosViewController.pullToRefresh(_:)), for: .valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        tableView.dataSource = self
         fetchPhotos()
 
         // Do any additional setup after loading the view.
     }
 
+    @objc func pullToRefresh(_ refreshControl: UIRefreshControl){
+        
+        fetchPhotos()
+    }
+    
     func fetchPhotos(){
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -33,11 +47,35 @@ class PhotosViewController: UIViewController {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
                 let responseDictionary = dataDictionary["response"] as! [String : Any]
                 self.posts = responseDictionary["posts"] as! [[String : Any]]
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
         task.resume()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        
+        let post = posts[indexPath.row]
+        if let photos = post["photos"] as? [[String: Any]] {
+            // photos is NOT nil, we can use it!
+            // TODO: Get the photo url
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            
+            cell.photoImageView.af_setImage(withURL: url!)
+        }
+        
+        
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ tumblr/tumblr_demo/tumblr_demo/PostCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostCell.swift
+//  tumblr_demo
+//
+//  Created by Milind Siddhanti on 9/10/18.
+//  Copyright © 2018 Milind Siddhanti. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
