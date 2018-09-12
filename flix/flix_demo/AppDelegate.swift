e1122efb49bd2252f0ae29032596eb6364224f80
Milind Siddhanti <milindsiddhanti@utexas.edu>
2018-09-11 19:59:01 -0500

    adding files

--- flix/flix_demo/AppDelegate.swift
+++ flix/flix_demo/AppDelegate.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  AppDelegate.swift
 //  flix_demo
 //
---
+++ flix/flix_demo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo
+//
+//  Created by Milind Siddhanti on 9/11/18.
+//  Copyright © 2018 Milind Siddhanti. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
